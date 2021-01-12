7999b0cd96ed69e5bdf6dc4c945b084caef5c9d2
zaid <khanzaid786786.kz@gmail.com>
2021-01-12 19:27:57 +0530

    Updates

---
+++ precache-manifest.cfa294943d16b9792f930c1ca2badef2.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ede7cae48268070546f",
+    "url": "/portfolio/static/js/main.5b9b023d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "7d56fc3402932f56302a",
+    "url": "/portfolio/static/js/2.1ad3abfe.chunk.js"
+  },
+  {
+    "revision": "84e8cde467024333f7cdf9857a499049",
+    "url": "/portfolio/static/media/profile.84e8cde4.jpg"
+  },
+  {
+    "revision": "5b7732aec389e65167f54260dead10e6",
+    "url": "/portfolio/static/media/img_bg_1.5b7732ae.jpg"
+  },
+  {
+    "revision": "5c42adf0e09eaffff0ec091d58e91252",
+    "url": "/portfolio/static/media/img_bg_2.5c42adf0.jpg"
+  },
+  {
+    "revision": "4a2b3d82b5ad9bc89471f6b5aa2adf84",
+    "url": "/portfolio/static/media/oneAim-website.4a2b3d82.png"
+  },
+  {
+    "revision": "9419fbb16c65f13b528f791473424a07",
+    "url": "/portfolio/static/media/otopark-website.9419fbb1.png"
+  },
+  {
+    "revision": "bc632a6ebca79f586e323897a4c7aa26",
+    "url": "/portfolio/static/media/merchant-portal.bc632a6e.png"
+  },
+  {
+    "revision": "52e770e5d33435c758f679dbeed8371d",
+    "url": "/portfolio/static/media/otopark-admin-portal.52e770e5.png"
+  },
+  {
+    "revision": "e449c062b46c18a546168a933623fafc",
+    "url": "/portfolio/static/media/chat-app.e449c062.png"
+  },
+  {
+    "revision": "c98f045b1e53a992aca197f9c37ef8e3",
+    "url": "/portfolio/static/media/todo-app.c98f045b.png"
+  },
+  {
+    "revision": "42f9b69133324d82fea5e95ac986cf0f",
+    "url": "/portfolio/static/media/bewakoof-app.42f9b691.png"
+  },
+  {
+    "revision": "237bf9fe1a61e8065fa14fcbb58c8e4b",
+    "url": "/portfolio/static/media/insta-app.237bf9fe.png"
+  },
+  {
+    "revision": "aeb0ab8c09360843858d9916714c5fc4",
+    "url": "/portfolio/static/media/plvm-app.aeb0ab8c.png"
+  },
+  {
+    "revision": "7cb5fb7fde45f0710956c69cbb9e6301",
+    "url": "/portfolio/static/media/parking-web-app.7cb5fb7f.png"
+  },
+  {
+    "revision": "606abee6737bb1c504358392a26d259b",
+    "url": "/portfolio/static/media/cover_bg_1.606abee6.jpg"
+  },
+  {
+    "revision": "bae40da01af3c73d53fe56c6992d3247",
+    "url": "/portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.c0fd32160955dbbff310350978b9250c.js"
+  "/portfolio/precache-manifest.cfa294943d16b9792f930c1ca2badef2.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute("/portfolio/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
